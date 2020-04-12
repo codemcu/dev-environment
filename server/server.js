a534b0c977e690ec2946a03fd2d06f582e836dd1
Mauricio Correa <frontend.mcu@gmail.com>
2020-04-12 13:03:27 +0200

    webpack configuration in the server file and in the .config.dev file

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const path = require("path");
 const open = require("open");
+import webpack from "webpack";
+import config from "./../webpack.config.dev";
 
 const app = express();
 const port = 8081;
+const compiler = webpack(config);
+
+app.use(
+  require("webpack-dev-middleware")(compiler, {
+    publicPath: config.output.publicPath,
+  })
+);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../src/index.html"));
---
+++ webpack.config.dev.js
@@ -0,0 +1,17 @@
+import path from "path";
+
+export default {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: [path.resolve(__dirname, "src/index")],
+  target: "web",
+  output: {
+    path: path.resolve(__dirname, "src"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  plugins: [],
+  module: {
+    rules: [{ test: /\.css$/, use: ["style-loader", "css-loader"] }],
+  },
+};
